ded0029f07984ba78302fc2ef692a74feed0ece7
FurkannnAtes <aliriza20005@hotmail.com>
2023-02-28 18:20:04 +0300

    redux toolkit connect

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -4,9 +4,17 @@ import { GoogleLogin } from "@react-oauth/google";
 import { FaTiktok } from "react-icons/fa";
 import { AiOutlinePlus } from "react-icons/ai";
 import { BsSearch } from "react-icons/bs";
+import { useDispatch, useSelector } from "react-redux";
+import { createOrGetUser, logout } from "../../store/Auth";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [authBtn, setAuthBtn] = useState(false);
+  const [profileBtn, setProfileBtn] = useState(false);
+
+  const user = useSelector((state) => state.user.user);
+  const dispatch = useDispatch();
+
   return (
     <div className=" border-b  border-[1px]">
       <div className="wrapper flex justify-between items-center px-5 h-[9vh]">
@@ -26,17 +34,40 @@ const Navbar = () => {
             </button>
           </div>
         </div>
+
         <div className="flex items-center gap-2">
           <button className="flex gap-2 justify-center items-center font-semibold border py-2 w-[130px] hover:bg-gray-200 duration-300">
             <AiOutlinePlus />
             <div>Upload</div>
           </button>
-          <button
-            onClick={() => setAuthBtn(authBtn ? false : true)}
-            className="flex justify-center gap-2 items-center font-semibold text-white bg-mainRed border py-2 w-[130px] hover:bg-[#d11e3c] duration-300"
-          >
-            <div>Sing in</div>
-          </button>
+          {user.name ? (
+            <div onMos className="relative">
+              <img
+                onClick={() => setProfileBtn(profileBtn ? false : true)}
+                className="w-10 h-10 rounded-full cursor-pointer"
+                src={user.picture}
+                alt=""
+              />
+              <div
+                className={`${
+                  profileBtn ? "flex" : "hidden"
+                } absolute top-full left-1/2 -translate-x-1/2 z-30 p-2 border bg-white flex flex-col gap-2 w-[100px]`}
+              >
+                <Link to="/">GO Profile</Link>
+                <div>
+                  {" "}
+                  <button onClick={() => dispatch(logout())}>Log out</button>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <button
+              onClick={() => setAuthBtn(authBtn ? false : true)}
+              className="flex justify-center gap-2 items-center font-semibold text-white bg-mainRed border py-2 w-[130px] hover:bg-[#d11e3c] duration-300"
+            >
+              <div>Sing in</div>
+            </button>
+          )}
         </div>
       </div>
       <div
@@ -48,7 +79,9 @@ const Navbar = () => {
         <div className="bg-white p-5 rounded-lg">
           <GoogleLogin
             onSuccess={(credentialResponse) => {
-              console.log(credentialResponse);
+              dispatch(createOrGetUser(credentialResponse));
+              setAuthBtn(false);
+              setProfileBtn(false);
             }}
             onError={() => {
               console.log("Login Failed");
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import Post from "../components/home/Post";
 import SideBar from "../components/home/SideBar";
 
@@ -6,6 +7,7 @@ const Home = () => {
   return (
     <div className="wrapper flex justify-between md:px-5 ">
       <SideBar />
+
       <div className="w-11/12 md:w-7/12 pt-5 px-1 flex flex-col h-[89vh] overflow-y-auto posts">
         <Post />
         <Post />
